a004ae4d25a57764185f428bf9dc9c7b4c7835b7
mohammed <medo9401466@gmail.com>
2023-09-29 10:04:11 -0700

    first

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - write chr c to stdout
+ * @c: is chr to print
+ *
+ * Description: 3aw
+ *
+ * Return: 1 or -1
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &, 1));
+}
